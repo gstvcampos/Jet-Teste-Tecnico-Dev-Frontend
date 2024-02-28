2245febf0aaab405a0bb487e2a9acf2685789428
gstvcampos <gustavo.bmc76@gmail.com>
2024-02-28 18:46:35 -0300

    feat: estrutura da tabela

--- test-jet/src/app/page.tsx
+++ test-jet/src/app/page.tsx
@@ -1,6 +1,52 @@
-export default function Home() {
-  return <div className="btn btn-circle fixed flex">
+import Image from 'next/image'
 
-    
-  </div>
+export default function Home() {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>CAR</th>
+            <th>NEXT RESERVATION</th>
+            <th>STATUS</th>
+            <th>RATING</th>
+            <th>ACTIONS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask h-12 w-12">
+                    <Image
+                      src="/mini-cooper.png"
+                      width={500}
+                      height={500}
+                      alt="Picture of the author"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold">Mini Cooper 2020</div>
+                </div>
+              </div>
+            </td>
+            <td>
+              Zemlak, Daniel and Leannon
+              <br />
+              <span className="badge badge-ghost badge-sm">
+                Desktop Support Technician
+              </span>
+            </td>
+            <td>Purple</td>
+            <th>
+              <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
