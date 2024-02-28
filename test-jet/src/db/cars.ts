763eedc4d48e7155eefd57fc5991bf2c757913ed
gstvcampos <gustavo.bmc76@gmail.com>
2024-02-28 19:21:03 -0300

    feat: data base

---
+++ test-jet/src/db/cars.ts
@@ -0,0 +1,44 @@
+export const cars = [
+  {
+    id: '1',
+    name: 'Mini Cooper 2020',
+    nextReservation: '10/07',
+    status: 'Available',
+    rating: 2,
+  },
+  {
+    id: '2',
+    name: 'Celta 2000',
+    nextReservation: '06/07',
+    status: 'Available',
+    rating: 3,
+  },
+  {
+    id: '3',
+    name: 'Fiat 147',
+    nextReservation: '02/03',
+    status: 'Unavailable',
+    rating: 2,
+  },
+  {
+    id: '4',
+    name: 'Marea Turbo 2007',
+    nextReservation: '05/03',
+    status: 'Unavailable',
+    rating: 5,
+  },
+  {
+    id: '5',
+    name: 'Opala Turbo 1978',
+    nextReservation: '10/07',
+    status: 'Available',
+    rating: 3,
+  },
+  {
+    id: '6',
+    name: 'Fiat Uno 2013',
+    nextReservation: '02/03',
+    status: 'Unavailable',
+    rating: 4,
+  },
+]
